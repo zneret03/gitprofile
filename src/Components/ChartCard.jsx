40e99315f7c4d5549387397e8cb04e3ee9d765ed
Ian <DrilonIan@yahoo.com>
2020-06-22 20:27:55 +0800

    Initial

--- src/Components/ChartCard.jsx
+++ src/Components/ChartCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const ChartCard = props =>{
     return(
+        //ho
         <div className="shadow-lg rounded p-5 xl:p-5 lg: mt-5 mx-2 bg-white">
             {props.children}
         </div>
