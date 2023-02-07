a86b38eafac84eaca9e1b353753d75f971c187b0
zneret03 <drilonian@yahoo.com>
2023-02-07 16:58:34 +0800

    fix: fixing require images

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,30 +1,37 @@
-import React from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
 const Main = (props) => {
-    //const context = React.useContext(MainContext);
+  //const context = React.useContext(MainContext);
 
-    //icons
-    const github = <FontAwesomeIcon className="block text-blue-500 mb-8 mx-auto" icon={faGithub} size="4x"/>
-    return(
-        <div>
-            <div className="bg-black h-screen flex items-center justify-center">
-                <div className="mb-5">
-                    <div className="absolute top-0 right-0">
-                        <a href="https://github.com/zneret03/gitprofile">
-                            <img className="absolute right-0 py-2 px-3 z-50" 
-                            src={require('../images/github.svg')} alt="img"/>
-                        </a>
-                        <img src={require('../images/triangleShape.svg')} alt="img"/>
-                    </div>
-                    <div>
-                        {github}
-                    </div>
-                   {props.children}
-                </div>
-            </div>
+  //icons
+  const github = (
+    <FontAwesomeIcon
+      className="block text-blue-500 mb-8 mx-auto"
+      icon={faGithub}
+      size="4x"
+    />
+  )
+  return (
+    <div>
+      <div className="bg-black h-screen flex items-center justify-center">
+        <div className="mb-5">
+          <div className="absolute top-0 right-0">
+            <a href="https://github.com/zneret03/gitprofile">
+              <img
+                className="absolute right-0 py-2 px-3 z-50"
+                src="./images/github.svg"
+                alt="img"
+              />
+            </a>
+            <img src="./images/triangleShape.sv" alt="img" />
+          </div>
+          <div>{github}</div>
+          {props.children}
         </div>
-    );
+      </div>
+    </div>
+  )
 }
 
-export default Main;
+export default Main
--- src/page/Following.jsx
+++ src/page/Following.jsx
@@ -1,44 +1,53 @@
-import React from 'react'
-import FollowersCard from '../Components/FollowersCard'
-const Followers = ({modal, following}) => {
-    return(
-        <>                                                                  
-           <FollowersCard>
-                    {/**Modal content */}
-                    <div className="text-center pt-4 px-6">
-                        {/**Modal header */}
-                        <div className="pb-1">
-                            <div className="cursor-pointer float-right">
-                                <img src={require('../images/window_close.svg')} onClick={modal} alt=""/>
-                            </div>
-                            <p className="text-2xl font-bold tracking-wide">Following</p>
-                        </div>
-                    </div>
-                    {/**Modal body */}
-                    {following.length > 0 ? (
-                        <>
-                         {following.map(follow => (
-                            <div className="container mx-auto px-2">
-                              <div className="flex items-center my-5">
-                                  <a href={follow.html_url}>
-                                  <img className="rounded-full object-cover w-10 h-10 mx-4 border-2 border-blue-400"  src={`https://avatars3.githubusercontent.com/u/${follow.id}?v=4`} alt=""/>
-                                  </a>
-                                  <div className="text-sm flex-1 overflow-hidden">
-                                      <p className="font-bold leading-none sm:break-normal">{follow.login}</p>
-                                        <p className="text-gray-600">{follow.id}</p>                               
-                                  </div>
-                                  <div className="self-end mr-5 text-sm">
-                                      <button className="border py-2 px-4 rounded-sm font-bold">Following</button>
-                                  </div>
-                              </div>
-                          </div>
-                        ))}
-                        </>
-                    ) : (<p className="text-1xl text-gray-500 p-5">There is no Followers</p>)}
-                   
-            </FollowersCard>
-        </>
-    )
+import React from "react"
+import FollowersCard from "../Components/FollowersCard"
+const Followers = ({ modal, following }) => {
+  return (
+    <>
+      <FollowersCard>
+        {/**Modal content */}
+        <div className="text-center pt-4 px-6">
+          {/**Modal header */}
+          <div className="pb-1">
+            <div className="cursor-pointer float-right">
+              <img src="./images/window_close.svg" onClick={modal} alt="" />
+            </div>
+            <p className="text-2xl font-bold tracking-wide">Following</p>
+          </div>
+        </div>
+        {/**Modal body */}
+        {following.length > 0 ? (
+          <>
+            {following.map((follow) => (
+              <div className="container mx-auto px-2">
+                <div className="flex items-center my-5">
+                  <a href={follow.html_url}>
+                    <img
+                      className="rounded-full object-cover w-10 h-10 mx-4 border-2 border-blue-400"
+                      src={`https://avatars3.githubusercontent.com/u/${follow.id}?v=4`}
+                      alt=""
+                    />
+                  </a>
+                  <div className="text-sm flex-1 overflow-hidden">
+                    <p className="font-bold leading-none sm:break-normal">
+                      {follow.login}
+                    </p>
+                    <p className="text-gray-600">{follow.id}</p>
+                  </div>
+                  <div className="self-end mr-5 text-sm">
+                    <button className="border py-2 px-4 rounded-sm font-bold">
+                      Following
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </>
+        ) : (
+          <p className="text-1xl text-gray-500 p-5">There is no Followers</p>
+        )}
+      </FollowersCard>
+    </>
+  )
 }
 
-export default Followers;
+export default Followers
